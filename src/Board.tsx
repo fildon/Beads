e31bce925a3f404e4f3625597f794de973e532b8
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-04-05 21:57:58 +0200

    Display a message above the table about the game state

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { playMove, startingState } from "./gameEngine/gameState";
 
+const getDisplayMessage = (state: State): string => {
+  if (state.phase === "⏹") {
+    return "It's a tie!";
+  }
+
+  if (state.phase === "▶") {
+    return `${state.nextToMove} to play`;
+  }
+
+  return `${state.phase} has won!`;
+};
+
 const columnIndices: ColumnIndex[] = [0, 1, 2, 3, 4, 5, 6];
 export const Board = (): JSX.Element => {
   const [state, setState] = React.useState(startingState);
@@ -14,7 +26,7 @@ export const Board = (): JSX.Element => {
 
   return (
     <>
-      <h2>{state.nextToMove} to play</h2>
+      <h2>{getDisplayMessage(state)}</h2>
       <table>
         <thead>
           <tr>
