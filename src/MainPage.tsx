52b6d3bf8afbfc7ac2b8af3205f8318ee8747d30
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-04-10 08:46:40 +0200

    Deployment process

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./mainPage.module.css";
+import * as styles from "./mainPage.module.css";
 import { Board } from "./Board";
 
 const testState: State = {
