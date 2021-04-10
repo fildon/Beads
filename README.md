c3e3a4e92f20e94467bf06c072292984570708c4
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-04-10 08:51:16 +0200

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# Beads
-
-A connect4 implementation
-
-## Getting Started
-```bash
-yarn install
-yarn start
-```
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import { MainPage } from "./MainPage";
-
-render(<MainPage />, document.getElementById("root"));
