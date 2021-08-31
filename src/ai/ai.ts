91e68cb4eb2f07e3f1e534498fd5b2f7b4379df7
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-08-31 21:34:57 +0200

    1️⃣ depend on rusty-bot v1

--- src/ai/ai.ts
+++ src/ai/ai.ts
@@ -1,7 +1,11 @@
 import { pick_best_move } from "rusty-bot";
 
 export const pickBestMove = (state: GameState): ColumnIndex => {
-  const bestmove = pick_best_move(...state.bitboard);
-  console.log(`bot chose: ${bestmove}`);
+  const bestmove = pick_best_move(
+    state.bitboard[0],
+    state.bitboard[1],
+    8,
+    false
+  );
   return bestmove as ColumnIndex;
 };
